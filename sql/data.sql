4997aa36629089561129942b3f158672a0179409
Wei Junjie <you@example.com>
2019-01-27 09:46:44 +0800

    添加sql

--- sql/data.sql
+++ sql/data.sql
@@ -8,6 +8,17 @@ CREATE TABLE `movies` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `user` varchar(255) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `avatar` varchar(255) NOT NULL,
+  `content` varchar(511) CHARACTER SET utf8 DEFAULT NULL,
+  `images` varchar(1023) DEFAULT NULL,
+  `movie_id` int(11) NOT NULL,
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 SET @IMAGE_BASE_URL = "https://film-1258073751.cos.ap-shanghai.myqcloud.com/"; -- FOR EXAMPLE: https://*****.ap-shanghai.myqcloud.com/
 
 INSERT INTO `movies` (`id`, `image`, `title`, `category`, `description`) VALUES
